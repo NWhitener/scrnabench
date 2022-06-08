f339c0cc222bb1d8206a53fbb38054099f04aa63
Nathan Whitener <whitennp@wfu.edu>
2022-06-08 09:07:40 -0400

    Version changes

--- R/validate.R
+++ R/validate.R
@@ -29,7 +29,7 @@ run_silhouette <- function(dataList, reduction_choosen = 'pca', method = 'kmeans
       y = Seurat::Embeddings(dataList[[i]], reduction = reduction_choosen)
       z <- cluster::silhouette(x, dist(y, "euclidean"))
       q = summary(z)
-      print(i)
+      print(i, q)
       temp_list = cbind(temp_list, q$avg.width)
     }
     else{
