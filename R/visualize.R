a2c182e815ece738e705604e90dd6d59032c5130
Nathan Whitener <whitennp@wfu.edu>
2022-06-20 12:22:33 -0400

    visualize.R First Version Final Changes

--- R/visualize.R
+++ R/visualize.R
@@ -3,17 +3,12 @@
 #' Produces a plot of the clustering, based on the dimensionality reduction method chosen. The plot is a Seurat DimPlot
 #'
 #' @param data An individual data object that you want to visualize
-#' @param reductionChoosen The dimensionality reduction method choosen
-#' @param method The cluster method to use
+#' @param reductionTypeThe dimensionality reduction method chosen, defaults to PCA
+#' @param method The cluster method to use, defaults to Kmeans
 #' @export
-plot_clusters <- function(data, reductionChoosen = 'pca', method = 'kmeans'){
-  if(method == 'kmeans')
-  {
-  reductionType = paste('kmeans_cluster_', reductionChoosen, sep = '')
-  }
-  else {
-    reductionType = 'seurat_clusters'
-  }
-  return(Seurat::DimPlot(data, reduction = reductionChoosen, group.by = reductionType))
+plot_clusters <- function(data, reductionType = 'pca', method = 'kmeans')
+{
+  annotationField <- toupper(paste(method, '_cluster_', reductionType, sep=''))
+  return(Seurat::DimPlot(data, reduction = reductionType, group.by = annotationField))
 }
 
