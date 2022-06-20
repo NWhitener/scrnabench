0a40e863c5e39af1197b804f3f0b8379411f8d7f
Nathan Whitener <whitennp@wfu.edu>
2022-06-20 10:33:23 -0400

    data.R First Version final changes

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-
 #' Downloads the data
 #'
 #' This function downloads the data that is used in the package for use in examples, and automatically loads the data for
-#' usage. Run this once when the data is not download, if the data is downloaded use the data_load function
+#' usage. Run this once when the data is not download, if the data is downloaded use the load_data function
+#'
 #' @param path The path of the download location to use
 #' @export
 download_data <- function(path = '.')
@@ -10,35 +10,37 @@ download_data <- function(path = '.')
   inborutils::download_zenodo(doi = "10.5281/zenodo.6617997", path = path)
   geneCounts <<- readRDS(file = paste(path, "/gene_counts.RDS", sep = ''))
   datasets <- ls(geneCounts)
+
   return(datasets)
 }
 
 
 #' Loads the data
 #'
-#' This function loads the data that is used in the package for use in examples
+#' This function loads the data that is used in the package for use in workflows, functions, and examples
 #' @param full Use the full dataset or the small demo dataset
-#' @param path The path of the load location to use
+#' @param path The path of the load location to use, if the full datasets is to be loaded
 #' @export
 load_data <- function(demo = FALSE, path = '.')
-{
-  ## Check the Download Path
-  path_full = paste(path, "/gene_counts.RDS", sep = '')
-  if(demo)
   {
-    geneCounts <<- readRDS(system.file("extdata", "gene_counts_demo.RDS", package = "scrnabench", mustWork = TRUE))
+    ## Check the Download Path
+    path_full = paste(path, "/gene_counts.RDS", sep = '')
+    if(demo)
+    {
+      geneCounts <<- readRDS(system.file("extdata", "gene_counts_demo.RDS", package = "scrnabench", mustWork = TRUE))
+    }
+    else if(file.exists(path_full))
+    {
+      geneCounts <<- readRDS(file = path_full)
+    }
+    else
+    {
+      stop("Please Provide a Valid File Path, or use the download_data() function to download the data")
+    }
+    datasets <- ls(geneCounts)
 
+    return(datasets)
   }
-  else if(file.exists(path_full)){
-
-    geneCounts <<- readRDS(file = path_full)
-  }
-  else{
-    stop("Please Provide a Valid File Path, or use the data_download() function to download the data")
-  }
-  datasets <- ls(geneCounts)
-  return(datasets)
-}
 
 #' Merge the Sparse data sets
 #'
@@ -57,7 +59,8 @@ merge_datasets <- function(dataList)
   i <- numeric()
   j <- numeric()
 
-  for (data in dataList) {
+  for (data in dataList)
+  {
 
     cnold <- colnames(data)
     rnold <- rownames(data)
@@ -73,46 +76,41 @@ merge_datasets <- function(dataList)
     j <- c(j,cindnew[ind[,2]])
     x <- c(x,ind[,3])
   }
-  merged <- Matrix::sparseMatrix(i=i,j=j,x=x,dims=c(length(rnnew),length(cnnew)),dimnames=list(rnnew,cnnew))
-  merged<-list(merged)
-  names(merged)<-c("Integrated")
+  merged <- list(Matrix::sparseMatrix(i=i,j=j,x=x,dims=c(length(rnnew),length(cnnew)),dimnames=list(rnnew,cnnew)))
+  names(merged) <- c("Integrated")
+
   return(merged)
 }
 
-
-
-
-#' View Data
+#' Describe Datasets
 #'
 #' This function displays a short summary of each data set in the data list provided. Can be used to gain quick insights into the
 #' information that is stored in the data
 #'
 #' @param dataList A list of names of data sets that you would like to view
 #' @export
-view_data <- function(dataList)
+describe_datasets <- function(dataList)
 {
-
 
   # Show the dataset name, number of genes, number of cells
 
-  table = NULL
+  summaryTable = NULL
   for (i in 1:length(names(dataList)))
   {
-    name = names(dataList[i])
-    rows <- as.numeric(nrow(dataList[[i]]))
-    cols <- as.numeric(ncol(dataList[[i]]))
-    table <- rbind(table,cbind(name, rows, cols))
-
-
+    datasetNames = names(dataList[i])
+    numberGenes <- as.numeric(nrow(dataList[[i]]))
+    numberCells <- as.numeric(ncol(dataList[[i]]))
+    summaryTable <- rbind(summaryTable, cbind(datasetNames, numberGenes, numberCells))
   }
-  colnames(table) <- c("Dataset Name","Number of Genes", 'Number of Cells')
-  as.data.frame(table)
+  colnames(summaryTable) <- c('Dataset Name', 'Number of Genes', 'Number of Cells')
+  print(as.data.frame(summaryTable))
+}
 
-  }
+
 
 #' Extract Datasets
 #'
-#' This function extracts a set of Data sets for further use in the workflow. Each data set that is extracted will be a dgCMatrix
+#' This function extracts a set of Data sets for further use in the workflows. Each data set that is extracted will be a dgCMatrix
 #' with rows representing genes and cells representing columns
 #'
 #' @param names A list of names representing the data sets you would like to extract
@@ -155,40 +153,40 @@ annotate_datasets <- function(dataList)
       cols = colnames(x)
       for (i in (1:length(cols)))
       {
-      col <- cols[i]
-      col <- stringr::str_split(col, "-")[[1]][1]
-      x@meta.data$ID[i] <- col
+        col <- cols[i]
+        col <- stringr::str_split(col, "-")[[1]][1]
+        x@meta.data$ID[i] <- col
 
-      chunks <- stringr::str_split(col, "_")
-      x@meta.data$TECHNLOGY[i] <- chunks[[1]][1]
+        chunks <- stringr::str_split(col, "_")
+        x@meta.data$TECHNLOGY[i] <- chunks[[1]][1]
 
-      if(chunks[[1]][2] == "PE" | chunks[[1]][2] == "SE")
-      {
-        x@meta.data$CENTER[i] <- "TBU"
-      }
-      else
-      {
-        x@meta.data$CENTER[i] <- chunks[[1]][2]
-      }
+        if(chunks[[1]][2] == "PE" | chunks[[1]][2] == "SE")
+        {
+          x@meta.data$CENTER[i] <- "TBU"
+        }
+        else
+        {
+          x@meta.data$CENTER[i] <- chunks[[1]][2]
+        }
 
-      if(chunks[[1]][3] == "M" | chunks[[1]][3] == "HT")
-      {
-        x@meta.data$CELL_LINE[i] <- ifelse(chunks[[1]][4] == "A", 'HCC1395', 'HCC1395BL')
-        x@meta.data$PREPROCESS[i] <- chunks[[1]][5]
+        if(chunks[[1]][3] == "M" | chunks[[1]][3] == "HT")
+        {
+          x@meta.data$CELL_LINE[i] <- ifelse(chunks[[1]][4] == "A", 'HCC1395', 'HCC1395BL')
+          x@meta.data$PREPROCESS[i] <- chunks[[1]][5]
+        }
+        else
+        {
+          x@meta.data$CELL_LINE[i] <- ifelse(chunks[[1]][3] == "A", 'HCC1395', 'HCC1395BL')
+          x@meta.data$PREPROCESS[i] <- chunks[[1]][4]
+        }
       }
-      else
-      {
-        x@meta.data$CELL_LINE[i] <- ifelse(chunks[[1]][3] == "A", 'HCC1395', 'HCC1395BL')
-        x@meta.data$PREPROCESS[i] <- chunks[[1]][4]
-      }
-     }
       x@meta.data$SID <- rep(1, length(x@meta.data$ID))
       x@meta.data$orig.ident <- x@meta.data$CELL_LINE
       x <- x
-      })
+    })
     for (i in range(1, length(dataList)))
     {
-        dataList[[i]]@meta.data$SID = rep(i, length(dataList[[i]]@meta.data$SID))
+      dataList[[i]]@meta.data$SID = rep(i, length(dataList[[i]]@meta.data$SID))
     }
   }
 
@@ -198,17 +196,29 @@ annotate_datasets <- function(dataList)
   return(dataList)
 }
 
+
+#' Add Clustering Annotations
+#'
+#' This function will add clustering annotations (i.e memberships) to the metadata of the Seurat Object of the matching data.
+#' Any clustering method can be used to obtain these memberships. The annotation name will include the clustering type
+#' and the cells cluster ID.
+#'
+#' @param dataList A list of data sets to add the clustering annotation too
+#' @param memberships A list of the cluster memberships of each cell, used to add the annotation
+#' @param clusteringName The name of the clustering method used to obtain the clustering membership
+#' @export
 add_clustering_annotation <- function(dataList, memberships, clusteringName)
 {
-
-  for (i in range(1, length(dataList)))
+  if(is.list(dataList) & is.list(memberships))
   {
-    meta = dataList[[i]]@meta.data
-    meta[paste(clusteringName, sep = "")] <- memberships
+    for (i in (1: length(names(dataList))))
+    {
+      dataList[[i]][[clusteringName]] <- memberships[i]
+    }
   }
-
-
-
-
-
+  else
+  {
+    stop("A list of datasets and memberships is required to annotate datasets")
+  }
+  return(dataList)
 }
