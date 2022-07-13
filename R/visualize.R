43104c4f160326e42457d497e993ef679179f8b4
Nathan Whitener <whitennp@wfu.edu>
2022-07-13 08:30:51 -0400

    Title added to the Graph

--- R/visualize.R
+++ R/visualize.R
@@ -6,9 +6,12 @@
 #' @param reductionType The dimensionality reduction method chosen, defaults to PCA
 #' @param method The cluster method to use, defaults to Kmeans
 #' @export
-plot_clusters <- function(data, reductionType = 'pca', method = 'kmeans')
+plot_clusters <- function(data, reductionType = 'pca', method = 'kmeans', transformationType = 'log')
 {
   annotationField <- toupper(paste(method, '_cluster_', reductionType, sep=''))
-  return(Seurat::DimPlot(data, reduction = reductionType, group.by = annotationField))
+  titlePlot =  paste(toupper(method), "_", toupper(reductionType), "_", toupper(transformationType), sep = "")
+  plot = Seurat::DimPlot(data, reduction = reductionType, group.by = annotationField) +
+    patchwork::plot_annotation(titlePlot)
+  return(plot)
 }
 
